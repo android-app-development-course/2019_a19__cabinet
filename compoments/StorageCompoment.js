03e983b91641783beaf77328bf0103b4aa919738
fengkx <liangkx8237@gmail.com>
2019-11-20 21:41:30 +0800

    feat: add and delete category by sqlite

--- compoments/StorageCompoment.js
+++ compoments/StorageCompoment.js
@@ -1,10 +1,11 @@
-import {useState} from 'react';
+import {useState, useEffect} from 'react';
 import * as React from 'react';
-import {Button} from 'react-native-paper';
-import {ScrollView, StyleSheet, View} from 'react-native';
+import {Button, Portal, Dialog, Paragraph} from 'react-native-paper';
+import {ScrollView, StyleSheet, View, Dimensions, TouchableWithoutFeedback} from 'react-native';
 import chunk from "../utils/chunck";
 import ActionButton from "react-native-action-button";
 import Icon from 'react-native-vector-icons/Ionicons';
+import {getAllCat, deleteCat} from "../storage/sqlite";
 
 
 const no_roud_conner = StyleSheet.create({
@@ -46,28 +47,78 @@ const actionButtonIconStyle = StyleSheet.create({
     }
 });
 
+const strings = {
+    DeleteCat: 'Delete category',
+    confirm: 'Confirm',
+}
+
 export default function (props) {
-    const [catergries, setCatergries] = useState([]);
-    for (let i = 0; i < 10; i++) {
-        catergries.push({
-            icon: 'camera',
-            text: '药品'
+    const debugArray = [];
+    const winHeight = Dimensions.get('window').height;
+    // for (let i = 0; i < 10; i++) {
+    //     debugArray.push({
+    //         icon: 'camera',
+    //         text: '药品'
+    //     })
+    // }
+    const [catergries, setCatergries] = useState(debugArray);
+    const [dialogVisble, setDialogVisble] = useState(false);
+    const [readyDel, setReadyDel] = useState({});
+
+    const handleCameraClick = getAllCat().then(res => {
+        console.log(res)
+    });
+
+    const delCat = () => {
+        deleteCat(readyDel.id);
+        const idx = catergries.indexOf(readyDel);
+        idx >= 0 && catergries.splice(idx, 1);
+        setDialogVisble(false);
+    };
+
+    useEffect(() => {
+        getAllCat().then(res => {
+            res && setCatergries(res.rows._array.map(i => {
+                return {icon: i.cat_icon, text: i.cat_name, id: i.cat_id}
+            }));
         })
-    }
+    });
     return (
         <View>
-            <ScrollView>
+            <Portal>
+                <Dialog
+                    visible={dialogVisble}
+                    onDismiss={() => setDialogVisble(false)}>
+                    <Dialog.Title>
+                        {strings.DeleteCat + ' ' + readyDel.text}
+                    </Dialog.Title>
+                    <Dialog.Content>
+                        <Paragraph>This is simple dialog</Paragraph>
+                    </Dialog.Content>
+                    <Dialog.Actions>
+                        <Button
+                            onPress={() => delCat()}
+                        >{strings.confirm}</Button>
+                    </Dialog.Actions>
+                </Dialog>
+            </Portal>
+            <ScrollView style={{minHeight: winHeight - 160}}>
                 {
                     chunk(catergries, 3).map((row, index) => (
                         <View key={'btn-group' + index} style={mainStyle.buttonRow}>
                             {
                                 row.map((item, index) => (
-                                    <View key={index}>
-                                        <Button icon="camera" mode="contained" contentStyle={[buttonStyle]}
-                                                compact={true} onPress={() => console.log(item.text)}>
-                                            {item.text + index}
-                                        </Button>
-                                    </View>
+                                    <TouchableWithoutFeedback key={index}
+                                                              onLongPress={() => {
+                                                                  setReadyDel(item);
+                                                                  setDialogVisble(true);
+                                                              }}
+                                                              onPress={() => console.log(item.text)}>
+                                        <Button icon={item.icon}
+                                                mode="contained"
+                                                contentStyle={[buttonStyle]}
+                                                compact={true}>{item.text}</Button>
+                                    </TouchableWithoutFeedback>
                                 ))
                             }
                         </View>
@@ -76,7 +127,7 @@ export default function (props) {
                 <View style={{height: 150, flex: 1}}/>
             </ScrollView>
             <ActionButton offsetY={30} buttonColor="rgba(231,76,60,1)">
-                <ActionButton.Item onPress={() => console.log(props)}>
+                <ActionButton.Item onPress={() => handleCameraClick}>
                     <Icon name="md-camera" style={actionButtonIconStyle.item}/>
                 </ActionButton.Item>
                 <ActionButton.Item onPress={() => props.navigation.navigate('addcat')}>
